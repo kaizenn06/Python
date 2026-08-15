4e0f541636960a195eddb52cf9dbac8a88075b62
Kaizen <haibui1362006hd@gmail.com>
2026-08-15 14:27:48 +0700

    Create PY01060 — TÍCH CHỮ SỐ - TỔNG CHỮ SỐ.py

---
+++ BIẾN VÀ KIỂU DỮ LIỆU ĐƠN GIẢN/PY01060 — TÍCH CHỮ SỐ - TỔNG CHỮ SỐ.py
@@ -0,0 +1,13 @@
+import math
+if __name__ == "__main__" :
+    t = int(input())
+    for _ in range(t) :
+        n = input()
+        add, mul = 0, 1
+        add = sum(int(n[i]) for i in range(1, len(n), 2))
+        # tao 1 list a chua cac so khac o o vi tri le
+        a = [int(n[i]) for i in range(0, len(n), 2) if n[i] != '0']
+        mul = math.prod(a) if a else 0
+        print(mul, add)
+        
+        

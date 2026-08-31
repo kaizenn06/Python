cc4a8da31f9e732299191aa1f3b2418115e8d4af
Kaizen <haibui1362006hd@gmail.com>
2026-08-31 09:41:59 +0700

    Create PY02040 — MA TRẬN - 2.py

---
+++ DANH SÁCH/PY02040 — MA TRẬN - 2.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__" :
+	n = int(input())
+	a = list(list(map(int, input().split())) for _ in range(n))
+	k = int(input())
+	sum1, sum2 = 0, 0
+	for i in range(n) :
+		for j in range(n) :
+			if i + j > n - 1 :
+				sum1 += a[i][j]
+			if i + j < n - 1 :
+				sum2 += a[i][j]
+	print("YES" if abs(sum1 - sum2) <= k else "NO")
+	print(abs(sum1 - sum2))
